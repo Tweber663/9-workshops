f6226d95fb2176bb0f31d04ab4b6cd497b4f25e2
Thomas Weber <tweber.1995@hotmail.com>
2024-03-05 18:30:07 +0100

    last save of the day

--- src/js/script.js
+++ src/js/script.js
@@ -6,57 +6,56 @@ const domRef = {
 };
 
 const handleRef = {
-    //Handlebar compiler function -> on which we use the '#template-book'
-    bookListHandle: Handlebars.compile(document.querySelector(domRef.bookTemp).innerHTML),
-}
+  //Handlebar compiler function -> on which we use the '#template-book'
+  bookListHandle: Handlebars.compile(document.querySelector(domRef.bookTemp).innerHTML),
+};
 
 
 // Modify render function to accept a callback
 const render = (callback) => {
-    for (let bookID in dataSource.books) {
-        const HTMLElem = handleRef.bookListHandle(dataSource.books[bookID]);
-        const DOMElem = utils.createDOMFromHTML(HTMLElem); 
-        document.querySelector(domRef.booksList).appendChild(DOMElem);  
-    }
-    // Call the callback function after rendering is complete
-    if (typeof callback === 'function') {
-        callback();
-    }
+  for (let bookID in dataSource.books) {
+    const HTMLElem = handleRef.bookListHandle(dataSource.books[bookID]);
+    const DOMElem = utils.createDOMFromHTML(HTMLElem); 
+    document.querySelector(domRef.booksList).appendChild(DOMElem);  
+  }
+  // Call the callback function after rendering is complete
+  if (typeof callback === 'function') {
+    callback();
+  }
 };
 
 //Respons for adding fav books
 const initActions = () => {
 
-    //Fav book storage
-    const favoriteBooks = []
+  //Fav book storage
+  const favoriteBooks = [];
 
-    //Book List wrapper
-    const bookList = document.querySelector(domRef.booksList);
+  //Book List wrapper
+  const bookList = document.querySelector(domRef.booksList);
 
-    //Cycling through each book
-    Array.from(bookList.children).forEach((book) => {
-    
-        //Listenting to double clik
-        book.addEventListener('dblclick', function(event) {
-            event.preventDefault();
-            //Adding a new class 
-            book.children[1].classList.toggle('favorite')
-            //Grabbing id number of each book
-            const dataId = book.children[1].getAttribute('data-id');
+  bookList.addEventListener('dblclick', function(event) {
+    event.preventDefault();
+    let targetEle = event.target.parentElement.parentElement;
+
+    if (targetEle.classList.contains('book__image')) {
+         //Adding a new class 
+    targetEle.classList.toggle('favorite')
+    //Grabbing id number of each book
+    const dataId = targetEle.getAttribute('data-id');
             
-            //Checks is the book alredy exsists in the array
-            if (!favoriteBooks.some(b => b.bookId === dataId)) {
-            //If NOT we add a new obj
-                favoriteBooks.push({bookId: dataId})
-            } else  {
-            //If DOES we slice / remove the exsisting one
-                favoriteBooks.splice(favoriteBooks[dataId], 1)
-            }
+    //Checks is the book alredy exsists in the array
+    if (!favoriteBooks.some(b => b.bookId === dataId)) {
+      //If NOT we add a new obj
+      favoriteBooks.push({bookId: dataId});
+    } else  {
+      //If DOES we slice / remove the exsisting one
+      favoriteBooks.splice(favoriteBooks[dataId], 1);
+    }
+    console.log(favoriteBooks);
 
-            console.log(favoriteBooks);
-        })
-    })
-}
+    }
+  });
+};
 // Call render with initActions as the callback
 render();
 initActions();
