cb6c844b98b83c4d95f98128757a2a147942c29e
Thomas Weber <tweber.1995@hotmail.com>
2024-03-05 17:20:45 +0100

    JS - books toggle and are added to fav arr

--- src/js/script.js
+++ src/js/script.js
@@ -10,21 +10,54 @@ const handleRef = {
     bookListHandle: Handlebars.compile(document.querySelector(domRef.bookTemp).innerHTML),
 }
 
-//Book rendering function
-const bookRendering = () => {
 
-//Grab 'dataSource' and cycle throught each book. 
-for (let bookID in dataSource.books) {
-
-    //Using the handlebars complier to tranfrom obj data into hmtl text
-    const HTMLElem = handleRef.bookListHandle(dataSource.books[bookID]);
-    //Using the external function we transform html text to DOM element
-    const DOMElem = utils.createDOMFromHTML(HTMLElem); 
-    //Uplaoding the DOM element to HTML 
-    document.querySelector(domRef.booksList).appendChild(DOMElem); 
-}
-
+// Modify render function to accept a callback
+const render = (callback) => {
+    for (let bookID in dataSource.books) {
+        const HTMLElem = handleRef.bookListHandle(dataSource.books[bookID]);
+        const DOMElem = utils.createDOMFromHTML(HTMLElem); 
+        document.querySelector(domRef.booksList).appendChild(DOMElem);  
+    }
+    // Call the callback function after rendering is complete
+    if (typeof callback === 'function') {
+        callback();
+    }
 };
 
+//Respons for adding fav books
+const initActions = () => {
 
-bookRendering();
+    //Fav book storage
+    const favoriteBooks = []
+
+    //Book List wrapper
+    const bookList = document.querySelector(domRef.booksList);
+
+    //Cycling through each book
+    Array.from(bookList.children).forEach((book) => {
+    
+        //Listenting to double clik
+        book.addEventListener('dblclick', function(event) {
+            event.preventDefault();
+            //Adding a new class 
+            book.children[1].classList.toggle('favorite')
+            //Grabbing id number of each book
+            const dataId = book.children[1].getAttribute('data-id');
+            
+            //Checks is the book alredy exsists in the array
+            if (!favoriteBooks.some(b => b.bookId === dataId)) {
+            //If NOT we add a new obj
+                favoriteBooks.push({bookId: dataId})
+            } else  {
+            //If DOES we slice / remove the exsisting one
+                favoriteBooks.splice(favoriteBooks[dataId], 1)
+            }
+
+            console.log(favoriteBooks);
+        })
+    })
+}
+// Call render with initActions as the callback
+render();
+initActions();
+
