4598c18a05a1b1acd1e2ba335ee78a222e54b336
Thomas Weber <tweber.1995@hotmail.com>
2024-03-06 22:00:15 +0100

    Last commit of the day

--- src/js/script.js
+++ src/js/script.js
@@ -12,110 +12,118 @@ const handleRef = {
   bookListHandle: Handlebars.compile(document.querySelector(domRef.bookTemp).innerHTML),
 };
 
-// Renders information from data.js file on the sreen
-const render = () => {
 
-  //Cycle throught each book obj 
-  for (let bookID in dataSource.books) {
-    const bookObj = dataSource.books[bookID]
-    
-    //Adding new data to renderd information 
-    //1. Backgroud color rating
-    bookObj.ratingBgc = determineRatingBgc(bookObj.rating); //Callback function ->
-    //2. Precante rating
-    bookObj.ratingWidth = parseInt(Math.round(bookObj.rating).toString().padEnd(2, 0));
+class BookList {
+  constructor() {
+    const thisElement = this; 
 
-    //change data from data.js to html using HB
-    const HTMLElem = handleRef.bookListHandle(bookObj);
-    //change html from HB to DOM elements
-    const DOMElem = utils.createDOMFromHTML(HTMLElem); 
-    //Render the DOM element to the html
-    document.querySelector(domRef.booksList).appendChild(DOMElem); 
-    
+    thisElement.render();
+    thisElement.initActionsFav();
+    thisElement.initActionsFiltr();
   }
-};
+  // Renders information from data.js file on the sreen
+  render() {
+     //Cycle throught each book obj 
+    for (let bookID in dataSource.books) {
+      const bookObj = dataSource.books[bookID]
+      
+      //Adding new data to renderd information 
+      //1. Backgroud color rating
+      bookObj.ratingBgc = determineRatingBgc(bookObj.rating); //Callback function ->
+      //2. Precante rating
+      bookObj.ratingWidth = parseInt(Math.round(bookObj.rating).toString().padEnd(2, 0));
 
-//Respons for adding fav books + prepares filters 
-const initActions = () => {
-
-  //Fav book storage
-  const favoriteBooks = [];
-
-  //Book List wrapper
-  const bookList = document.querySelector(domRef.booksList);
-
-  bookList.addEventListener('dblclick', function(event) {
-    event.preventDefault();
-    let targetEle = event.target.parentElement.parentElement;
-
-    if (targetEle.classList.contains('book__image')) {
-         //Adding a new class 
-    targetEle.classList.toggle('favorite')
-    //Grabbing id number of each book
-    const dataId = targetEle.getAttribute('data-id');
-            
-    //Checks is the book alredy exsists in the array
-    if (!favoriteBooks.some(b => b.bookId === dataId)) {
-      //If NOT we add a new obj
-      favoriteBooks.push({bookId: dataId});
-    } else  {
-      //If DOES we slice / remove the exsisting one
-      favoriteBooks.splice(favoriteBooks[dataId], 1);
+      //change data from data.js to html using HB
+      const HTMLElem = handleRef.bookListHandle(bookObj);
+      //change html from HB to DOM elements
+      const DOMElem = utils.createDOMFromHTML(HTMLElem); 
+      //Render the DOM element to the html
+      document.querySelector(domRef.booksList).appendChild(DOMElem); 
     }
-    determineRatingBgc();
-    }
-  });
-
-  //Stores info about what filters are currently selected
-  const filters = [];
-
-  const filterList = document.querySelector(domRef.filtersRef);
-  
-  //Event delegation event listener (listenting to wrapper filter list (radios)
-  filterList.addEventListener('change', function(event) {
-  if(event.target.type === 'checkbox' && event.target.checked) {
-    filters.push(event.target.value);
-  } else  if (!event.target.checked){
-    console.log('removed:', event.target.value)
-    let index = filters.indexOf(event.target.value);
-    filters.splice(index, 1);
   }
-  renderFilter(filters);
-  })
-}
 
-//Responsible for rending filtered
-const renderFilter = (filters) => {
+  //Respons for adding fav books + prepares filters 
+ initActionsFav() {
 
-  console.log(filters);
+    //Fav book storage
+    const favoriteBooks = [];
 
-  for (let bookId of dataSource.books) {
-    //Refrence to individual books
+    //Book List wrapper
+    const bookList = document.querySelector(domRef.booksList);
 
-  const bookref = document.querySelector('[data-id="' + bookId.id + '"]');
+    bookList.addEventListener('dblclick', function(event) {
+      event.preventDefault();
+      let targetEle = event.target.parentElement.parentElement;
 
-   let notSelected = false;
-
-   //We're getting the list of books that have NOT been under given criteria
-    for (let filter of filters) {
-      if (!bookId.details[filter]) {
-        notSelected = true;
-        console.log(bookId)
+      if (targetEle.classList.contains('book__image')) {
+          //Adding a new class 
+      targetEle.classList.toggle('favorite')
+      //Grabbing id number of each book
+      const dataId = targetEle.getAttribute('data-id');
+              
+      //Checks is the book alredy exsists in the array
+      if (!favoriteBooks.some(b => b.bookId === dataId)) {
+        //If NOT we add a new obj
+        favoriteBooks.push({bookId: dataId});
+      } else  {
+        //If DOES we slice / remove the exsisting one
+        favoriteBooks.splice(favoriteBooks[dataId], 1);
       }
-    } //notSelected = true every time loops end with a book NOT matching given ctrieria
+      determineRatingBgc();
+      }
+    });
 
-    // We enter into second verification process, wehre we tell the computer 
-    // If book has exited with notSelected === true, for sure doesn't match the crtieria
-    // So we hide this book
-    if (notSelected === true) {
-      bookref.classList.add('hidden');
-    } else {
-      bookref.classList.remove('hidden')
-    }
   }
-}
 
-const determineRatingBgc = (rating) => {
+initActionsFiltr() {
+   //Stores info about what filters are currently selected
+   const filters = [];
+
+   const filterList = document.querySelector(domRef.filtersRef);
+   
+   //Event delegation event listener (listenting to wrapper filter list (radios)
+   filterList.addEventListener('change', function(event) {
+     if(event.target.type === 'checkbox' && event.target.checked) {
+       filters.push(event.target.value);
+     } else  if (!event.target.checked){
+       console.log('removed:', event.target.value)
+       let index = filters.indexOf(event.target.value);
+       filters.splice(index, 1);
+     }
+   renderFilter(filters);
+   })
+  }
+
+  //Responsible for rending filtered
+  renderFilter(filters){
+
+    for (let bookId of dataSource.books) {
+      //Refrence to individual books
+
+    const bookref = document.querySelector('[data-id="' + bookId.id + '"]');
+
+    let notSelected = false;
+
+    //We're getting the list of books that have NOT been under given criteria
+      for (let filter of filters) {
+        if (!bookId.details[filter]) {
+          notSelected = true;
+          console.log(bookId)
+        }
+      } //notSelected = true every time loops end with a book NOT matching given ctrieria
+
+      // We enter into second verification process, wehre we tell the computer 
+      // If book has exited with notSelected === true, for sure doesn't match the crtieria
+      // So we hide this book
+      if (notSelected === true) {
+        bookref.classList.add('hidden');
+      } else {
+        bookref.classList.remove('hidden')
+      }
+    }
+ }
+
+determineRatingBgc(rating) {
   let gradient = '';
     if (rating > 9) {
       gradient = 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)'
@@ -127,9 +135,8 @@ const determineRatingBgc = (rating) => {
       gradient = 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%);'
     }
     return gradient
+  }
 }
 
 
-// Call render with initActions as the callback
-render();
-initActions();
+new BookList();
